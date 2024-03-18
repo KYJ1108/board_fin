a81a0b16fe02695b9dfc00ddc51b42048d16b153
unknown <kimyeji546408@gmail.com>
2024-03-18 17:21:00 +0900

    board_add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        ArrayList<String> titleList = new ArrayList<>();
+        ArrayList<String> bodyList = new ArrayList<>();
 
         while (true){ // 반복 조건이 true이기 때문에 무한 반복
             System.out.print("명령어 : ");
@@ -14,7 +18,17 @@ public class Main {
                 System.out.println("프로그램을 종료합니다.");
                 break; // 반복문 탈출
             }
-        }
+            else if(cmd.equals("add")){
+                System.out.print("게시물 제목을 입력해주세요 : ");
+                String title = scan.nextLine();
+                titleList.add(title); // titleList배열에 title을 추가
 
+                System.out.print("게시물 내용을 입력해주세요 : ");
+                String body = scan.nextLine();
+                bodyList.add(body);
+
+                System.out.println("게시물이 등록되었습니다.");
+            }
+        }
     }
 }
