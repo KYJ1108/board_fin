f45b317486026cfdff0e3de96cd1cd9dc79edb99
unknown <kimyeji546408@gmail.com>
2024-03-18 17:15:41 +0900

    board_exit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true){ // 반복 조건이 true이기 때문에 무한 반복
+            System.out.print("명령어 : ");
+            String cmd = scan.nextLine();
+
+            if (cmd.equals("exit")){ // 숫자가 아닌 경우 같다라는 표현을 할 때 == 이 아닌 .equals()를 사용해야 한다.
+                System.out.println("프로그램을 종료합니다.");
+                break; // 반복문 탈출
+            }
+        }
 
     }
 }
